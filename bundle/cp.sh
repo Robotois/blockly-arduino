94ae0f7b7a18f7fec27d2d24c3bae421708b745e
manduks <iam@armando.mx>
2019-09-28 17:42:20 -0500

    updates scripts

--- bundle/cp.sh
+++ bundle/cp.sh
@@ -1,2 +1,4 @@
-cp ../blockly/javascript_compressed.js ../blockly/blockly_compressed.js ../blockly/blocks_compressed.js ../blocklyduino/js/BlocklyDuino_code.js  ../blocklyduino/js/arduino_boards.js ./bundle/dist/arduino-bundle.js /Users/manduks/Documents/code/robotois-ui/public/blockly
+cp ../blockly/javascript_compressed.js ../blockly/blockly_compressed.js ../blockly/blocks_compressed.js ../blocklyduino/js/BlocklyDuino_code.js  ../blocklyduino/js/arduino_boards.js ./dist/arduino-bundle.js /Users/manduks/Documents/code/robotois-ui/public/blockly
 
+#http-server
+#npx webpack
